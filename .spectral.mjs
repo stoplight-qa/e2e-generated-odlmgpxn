6926ce1035f2542248ac0bf30d1bc20845c0ac14
qa-srjxg-2-flxi@example.com <qa-srjxg-2-flxi@example.com>
2023-02-08 15:58:58 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15181.stoplight-dev.com/api/v1/projects/cHJqOjIzMQ/spectral.js?branch_slug=main&token=1b8b7003-e6d9-42f6-86fa-e84429547350";
+export default { extends: ruleset };
